ef813cc85346047e6c13f5e41336e1f117114fa2
ChangmoSung <changmosung0914@gmail.com>
2020-05-31 04:30:39 -0400

    authentication token

--- src/routers/user.js
+++ src/routers/user.js
@@ -1,5 +1,6 @@
 const express = require('express')
 const User = require('../models/user')
+const auth = require('../middleware/auth')
 const router = new express.Router()
 
 router.post('/users', async (req, res) => {
@@ -7,7 +8,7 @@ router.post('/users', async (req, res) => {
 
     try {
         await user.save()
-        
+
         const token = await user.generateAutoToken()
 
         res.status(201).send({user, token})
@@ -27,14 +28,8 @@ router.post('/users/login', async (req, res) => {
     }
 }) 
 
-router.get('/users', async (req, res) => {
-    try {
-        const user = await User.find({})
-
-        res.send(user)
-    } catch (e) {
-        res.status(500).send(e)
-    }
+router.get('/users/me', auth, async (req, res) => {
+    res.send(req.user)
 })
 
 router.get('/users/:id', async (req, res) => {
