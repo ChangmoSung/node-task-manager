e4c1065f134dd8c42e417da567b125dc496bc81d
ChangmoSung <changmosung0914@gmail.com>
2020-06-01 04:14:31 -0400

    handling express errors

--- src/routers/user.js
+++ src/routers/user.js
@@ -100,6 +100,8 @@ const upload = multer({
 })
 router.post('/users/me/magicLand', upload.single('magicLand'), (req, res) => {
     res.send()
+}, (error, req, res, next) => {
+    res.status(400).send({ error: error.message })
 })
 
 module.exports = router
